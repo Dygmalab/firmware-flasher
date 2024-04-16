49663279932689d35e94dd66560ae9e68790c9e3
Alejandro Parcet Gonzalez <alexpargon@gmail.com>
2024-04-16 17:39:25 +0200

    changed try catch structure from the main js file

--- src/main.js
+++ src/main.js
@@ -30,7 +30,7 @@ import path from "node:path";
  */
 function validate() {
   let fileName = "";
-  let fullErase = true;
+  let fullErase = false;
   let args = process.argv.slice(2);
   if (
     args.length < 2 ||
@@ -46,7 +46,7 @@ function validate() {
   }
   fileName = args[0];
   console.log("Searching file to flash: ", fileName);
-  fullErase = args[1] === "false" || fullErase;
+  fullErase = args[1] === "true";
   console.log("Default erase full flash: ", fullErase);
   return { fileName, fullErase };
 }
@@ -54,59 +54,59 @@ function validate() {
 try {
   // Check if the input parameters are valid
   let { fileName, fullErase } = validate();
-} catch (error) {
-  // end execution
-  process.exit();
-}
 
-// Create focus and open it against the provided device
-console.log("searching for N2...");
-const delay = (ms) => new Promise((res) => setTimeout(res, ms));
-let focus = new Focus();
-let info = { usb: { productId: 0x0012, vendorId: 0x35ef } };
-let open = false;
-try {
-  open = await focus.open(info, info);
-} catch (e) {
-  console.error(e);
-}
-if (open !== false) {
-  await focus.request("upgrade.start");
-  let ready = false;
-  do {
-    let isReady = await focus.request("upgrade.isReady");
-    if (isReady !== "true ") {
-      console.log(
-        "Device not ready for upgrade disconnect all sides or is sides are connected press esc"
-      );
-      await delay(500);
-    } else {
-      ready = true;
-    }
-  } while (ready === false);
-
+  // Create focus and open it against the provided device
+  console.log("searching for N2...");
+  const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+  let focus = new Focus();
+  let info = { usb: { productId: 0x0012, vendorId: 0x35ef } };
+  let open = false;
   try {
-    await focus.request("upgrade.neuron");
+    open = await focus.open(info, info);
   } catch (e) {
     console.error(e);
   }
-}
-info = { usb: { productId: 0x0013, vendorId: 0x35ef } };
-if (focus.closed) open = await focus.open(info, info);
-
-// Begin the update process of the FW
-console.log("Begin update firmware with NRf52833...");
-try {
-  await NRf52833.flash(
-    fileName,
-    async (err, result) => {
-      if (err) throw new Error(`Flash error ${result}`);
-      else {
-        console.log("Firmware update of NRf52833 ended succesfully");
+  if (open !== false) {
+    await focus.request("upgrade.start");
+    let ready = false;
+    do {
+      let isReady = await focus.request("upgrade.isReady");
+      if (isReady !== "true ") {
+        console.log(
+          "Device not ready for upgrade disconnect all sides or is sides are connected press esc"
+        );
+        await delay(500);
+      } else {
+        ready = true;
       }
-    },
-    fullErase
-  );
-} catch (e) {
-  console.error("something went wrong!", e);
+    } while (ready === false);
+
+    try {
+      await focus.request("upgrade.neuron");
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  info = { usb: { productId: 0x0013, vendorId: 0x35ef } };
+  if (focus.closed) open = await focus.open(info, info);
+
+  // Begin the update process of the FW
+  console.log("Begin update firmware with NRf52833...");
+  try {
+    await NRf52833.flash(
+      fileName,
+      async (err, result) => {
+        if (err) throw new Error(`Flash error ${result}`);
+        else {
+          console.log("Firmware update of NRf52833 ended succesfully");
+        }
+      },
+      fullErase
+    );
+  } catch (e) {
+    console.error("something went wrong!", e);
+  }
+} catch (error) {
+  // end execution
+  process.exit();
 }
